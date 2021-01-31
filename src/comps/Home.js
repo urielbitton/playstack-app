4ead0c737aacedf86aecc61b7a1da129292cb432
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2021-01-31 15:30:59 -0500

    hero banner UI

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className="homepage hidescroll">
       <HeroBanner src="https://i.imgur.com/akxDcN6.jpg"/>
-      <div style={{height: "2000px"}}></div>
     </div>
   )
 }
